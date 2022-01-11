a4ec9bf636cad973fdfb736f098c61671e928532
Ben Tenzlinger <btenzlin@3-B-11.42heilbronn.de>
2022-01-11 17:53:57 +0100

    minor changes

--- main.c
+++ main.c
@@ -6,19 +6,25 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 12:16:20 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/01/11 17:11:44 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/01/11 17:45:26 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	check_input(t_clst **argv)
+{
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
-	t_clst	*last;
+	t_clst	*stack_a;
+	t_clst	*stack_b;
 
-	last = init_list(argc, argv);
-	print_list(last);
-	// last = last->next;
-	// print_list(last);
+	stack_a = init_list(argc, argv);
+	stack_b = NULL;
+	print_list(stack_a);
+	// system("leaks push_swap");
 	return (0);
 }
