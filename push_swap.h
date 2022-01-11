d44c2c2f95e10d0f4c2ee08e4a7ba4b0f4c93c6c
Ben Tenzlinger <btenzlin@3-B-11.42heilbronn.de>
2022-01-11 17:53:09 +0100

    added functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 11:39:09 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/01/11 17:09:50 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/01/11 17:23:07 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ typedef struct s_clst
 }					t_clst;
 
 t_clst	*insert_empty(t_clst *last, int value);
+t_clst	*insert_first(t_clst *last, int value);
 t_clst	*insert_last(t_clst *last, int value);
 void	print_list(t_clst *last);
 t_clst	*init_list(int argc, char **argv);
