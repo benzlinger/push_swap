9d1cb59b2e7e2320e2ebc6e47294c64ca272c946
Ben Tenzlinger <btenzlin@2-A-7.42heilbronn.de>
2022-01-13 14:54:02 +0100

    new fcts

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 11:39:09 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/01/11 17:23:07 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/01/13 14:37:56 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <stdbool.h>
 # include <unistd.h>
+# include <limits.h>
 
 typedef struct s_clst
 {
@@ -29,6 +30,10 @@ t_clst	*insert_empty(t_clst *last, int value);
 t_clst	*insert_first(t_clst *last, int value);
 t_clst	*insert_last(t_clst *last, int value);
 void	print_list(t_clst *last);
-t_clst	*init_list(int argc, char **argv);
+t_clst	*del_first(t_clst *last);
+t_clst	*init_list(char **argv);
+void	check_input(int argc, char **argv);
+void	swap(t_clst *last);
+t_clst	*push(t_clst *from, t_clst *to);
 
 #endif
