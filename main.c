9e2bed65500e0ccf337d25f2953aca5ffb74fdc7
Ben Tenzlinger <btenzlin@2-A-7.42heilbronn.de>
2022-01-13 14:52:56 +0100

    test operations

--- main.c
+++ main.c
@@ -6,25 +6,26 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 12:16:20 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/01/11 17:45:26 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/01/13 14:46:30 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	check_input(t_clst **argv)
-{
-	return (0);
-}
 
 int	main(int argc, char **argv)
 {
 	t_clst	*stack_a;
 	t_clst	*stack_b;
 
-	stack_a = init_list(argc, argv);
+	check_input(argc, argv);
+	stack_a = init_list(argv);
 	stack_b = NULL;
+	//stack_b = insert_first(stack_b, 8);
 	print_list(stack_a);
-	// system("leaks push_swap");
+	print_list(stack_b);
+	stack_a = push(stack_a, stack_b);
+	print_list(stack_a);
+	print_list(stack_b);
+	//system("leaks push_swap");		//remove when finished
 	return (0);
 }
